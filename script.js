ad303f7d200495207ffdd002d38521ba0dd3e2cd
VSF <47865897+HashiY@users.noreply.github.com>
2022-01-30 17:39:15 -0300

    Parte 5

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const dino = document.querySelector('.dino'); //para selecionar e manipular os elementos da tela
 const background = document.querySelector('.background');//nao pode ser sobreescrito
 
+let isJumping = false;
+
 function handleKeyUp(event) {
     if (event.keyCode === 32) {//space
-        jump();
+        if (!isJumping){
+            jump();
+        }
     }
 }
 
 function jump(){
+    isJumping = true;
     let position = 0;
 
     let upInterval = setInterval(() =>{ //defini intervalos
@@ -17,6 +22,7 @@ function jump(){
             let downInterval = setInterval(() =>{
                 if (position <= 0){
                     clearInterval(downInterval);// para de descer
+                    isJumping = false;
                 }else{
                     position -= 20;
                     dino.style.bottom = position + 'px';
@@ -27,9 +33,17 @@ function jump(){
             position += 20;
             dino.style.bottom = position + 'px'; 
         }
-        
     },20);
 }
 
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
 
+    cactus.classList.add('cactus');
+    background.appendChild(cactus); // adiciona o cactus
+    cactus.style.left = cactusPosition + 'px';
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
