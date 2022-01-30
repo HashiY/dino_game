affd05bdfef5f59fd04f5e0d1e9c6ac8bfaba1db
VSF <47865897+HashiY@users.noreply.github.com>
2022-01-30 17:26:11 -0300

    Parte 4

--- script.js
+++ script.js
@@ -3,8 +3,33 @@ const background = document.querySelector('.background');//nao pode ser sobreesc
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {//space
-      console.log('Pressionou espaço')
+        jump();
     }
-  }
+}
+
+function jump(){
+    let position = 0;
+
+    let upInterval = setInterval(() =>{ //defini intervalos
+        if (position >= 150){
+            clearInterval(upInterval); // para de subir
+            // Descendo
+            let downInterval = setInterval(() =>{
+                if (position <= 0){
+                    clearInterval(downInterval);// para de descer
+                }else{
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            },20);
+        }else{
+            // Subindo
+            position += 20;
+            dino.style.bottom = position + 'px'; 
+        }
+        
+    },20);
+}
+
 
 document.addEventListener('keyup', handleKeyUp);
